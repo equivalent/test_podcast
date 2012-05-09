8aba8ffb361a3de739b6c260e37afe25469b31f4
Tomas Valent <equivalent@eq8.eu>
2012-05-09 16:08:54 +0100

    initiating rails new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestPodcast::Application.config.secret_token = 'd3faa5b4e47ad4c450d8a360bdd6d64bd1aa7e7a61336a545a31cdb6f93fbbbc38eeac73fadae0f9c54cdbcbfe90dfbc6b138b90e915efb77ac718f7c10d55db'
