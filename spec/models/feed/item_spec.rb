5e139e60a12a2a76470f3a741a05200d81a4fef0
Tomas Valent <equivalent@eq8.eu>
2012-05-09 18:06:19 +0100

    refactoring item, failing spec for Feed::Item

--- spec/models/feed/item_spec.rb
+++ spec/models/feed/item_spec.rb
@@ -10,10 +10,11 @@
 describe Feed::Item do
   describe 'class methods'do
     describe 'initialize_from_entries' do
-      it 'should initialize new Feed::Item for each entry'
+      it 'should initialize new Feed::Item for each entry' do
         entries = [{:entry_one=>''},{ :entry_two=>''}]
         Feed::Item.initialize_from_entries(entries).should_receive(:new).twice
       end
+
     end
   end
 
@@ -21,9 +22,12 @@ describe Feed::Item do
   let(:item){demo_feed_pull.first}
 
   it 'should have title' do
-    item.title.should be_present
+    item.display_title.should be_present
+    item.display_title.should be "Behind The Iron Curtain 042 with UMEK"
   end
+
   it 'should have url' do
-    item.guid.should be_present
+    item.url.should be_present
+    item.url.should be "http://podcast.sixteenofive.com/UMEK-BTIC-042.mp3"
   end
 end
